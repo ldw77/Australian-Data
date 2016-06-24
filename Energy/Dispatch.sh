0648bd495878ba49f1ef2bee3ed2e36ab3dc9125
Liam Wagner <Liam.wagner@gmail.com>
2016-06-24 12:40:00 +1000

    Update Dispatch.sh

--- Energy/Dispatch.sh
+++ Energy/Dispatch.sh
@@ -8,3 +8,6 @@ done
 
 ###### Cut and paste dispatch data into a temp file called DONE.CSV
 awk 'BEGIN { FS=","; OFS="," } {$1=$3=$4=$6=$10=$11=$12=$13=$14=$15=$16=$17="";gsub(",+",",",$0)}1 {print $0}' "Dispatch_temp.CSV" >>"DONE.CSV"
+
+# The first column in the above file will be blank so we can remove it via the following
+awk -F, '{for(i=1;i<=NF;i++)if(i!=x)f=f?f FS $i:$i;print f;f=""}' x=1 DONE.CSV >> FINAL.CSV
